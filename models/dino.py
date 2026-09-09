b1f85a91b3c36d27b7b16445037ceffc254120ca
MoliaiELS <yguo704@connect.hkust-gz.edu.cn>
2026-09-09 17:11:43 +0800

    change dino encoder version to comply with python

--- models/dino.py
+++ models/dino.py
@@ -7,7 +7,7 @@ class DinoV2Encoder(nn.Module):
     def __init__(self, name, feature_key):
         super().__init__()
         self.name = name
-        self.base_model = torch.hub.load("facebookresearch/dinov2", name)
+        self.base_model = torch.hub.load("facebookresearch/dinov2:b48308a394a04ccb9c4dd3a1f0a4daa1ce0579b8", name)
         self.feature_key = feature_key
         self.emb_dim = self.base_model.num_features
         if feature_key == "x_norm_patchtokens":
